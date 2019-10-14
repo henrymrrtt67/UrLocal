9fce19ce418f717df113ade467b429f2c3b15d9e
Henry Merritt <henrymerritt@Henrys-MacBook-Pro-2.local>
2019-10-15 10:29:29 +1300

    finally got it working, now working with ASP.NET like I should have been in the first place

--- UrLocal/K-Nearest-Neighbour.cs
+++ UrLocal/K-Nearest-Neighbour.cs
@@ -3,8 +3,57 @@ namespace UrLocal
 {
     public class K_Nearest_Neighbour
     {
+        int craft = 0;
+        int complexity = 0;
+        int wineOrBeer = 0;
+        int[] checkboxes = null;
+        double priceRange = 100.0;
+
+        int best_bar_id = 0;
+        double best_bar_score = 100.0;
+
         public K_Nearest_Neighbour()
         {
+
+        }
+        public void training(int user_id)
+        {
+            UrLocalDB conn = new UrLocalDB();
+            (craft, complexity,wineOrBeer, checkboxes, priceRange)=conn.get_user_preferences(user_id);
+        }
+        public int testing(int bar_id, int barCraft, int complexities, int wineOrBeers,
+            int[] checkbox, double lqMealPrice, double lqBeerPrice, double uqMealPrice, double uqBeerPrice)
+        {
+            int craftScore = craft - barCraft;
+            if (craftScore<0) craftScore *= -1;
+            int compScore = complexity - complexities;
+            if (compScore<0) compScore *= -1;
+            int wineBeerScore = wineOrBeer - wineOrBeers;
+            if (wineBeerScore<0) wineBeerScore *= -1;
+            int checkboxScore = 0;
+            for (int i =0; i<checkbox.Length;i++)
+            {
+                if (checkboxes[i] != checkbox[i]) checkboxScore += 5;
+            }
+            double price = priceRange + lqMealPrice;
+            if (price < 0) price *= -1;
+            if (price<priceRange) priceRange=price;
+            price = priceRange + lqBeerPrice;
+            if (price < 0) price *= -1;
+            if (price < priceRange) priceRange = price;
+            price = priceRange + uqMealPrice;
+            if (price < 0) price *= -1;
+            if (price < priceRange) priceRange = price;
+            price = priceRange + uqBeerPrice;
+            if (price < 0) price *= -1;
+            if (price < priceRange) priceRange = price;
+            double totalScore = craftScore + compScore + wineBeerScore + checkboxScore + priceRange;
+            if (best_bar_score > totalScore)
+            {
+                best_bar_id = bar_id;
+                best_bar_score = totalScore;
+            }
+            return best_bar_id;
         }
     }
 }
